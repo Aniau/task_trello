16fd29696124c106eef1e3cb50d425f5ee54bb23
Anna Ulanecka <ania.ulanecka@gmail.com>
2019-07-17 21:47:37 +0200

    Delete unnecessary files and upper trello columns with only movable columns. Update necessary files.

--- zadanie.js
+++ zadanie.js
@@ -1,66 +1,19 @@
-			var SrcElement= null; 
-			function handleDragStart(e) 
-			{
-			  this.style.opacity = '1';  //  set opacity of the element for minimum value
-				SrcElement = this;
+// sort list function and connection sortable list between each other            
+			function placeholder(element) {
+                    return $("<li class='sortable' id='backgroundColor'</li>");
+                }
 
-			  e.dataTransfer.effectAllowed = 'move'; // allow elements for movement and making movement
-			  e.dataTransfer.setData('text/html', this.innerHTML);
-			}
-			function handleDragOver(e) 
-			{
-			  if (e.preventDefault) 
-			  {
-				e.preventDefault(); 
-			  }
+                $(document).ready(function() {
+                    $(".sortable_list").kendoSortable({
+                        connectWith: ".connectedSortable",
+                        placeholder: placeholder
+                    });
 
-			  e.dataTransfer.dropEffect = 'move';  // stop movement
+                });
+				
+				 $(document).ready(function() {
+                    $(".sortable_list1").kendoSortable({
+                        connectWith: ".connectedSortable1",
+                    });
 
-			  return false;
-			}
-
-			function handleDragEnter(e) 
-			{
-			  this.classList.add('over'); // adding new class during movement of the column
-			}
-
-			function handleDragLeave(e) 
-			{
-			  this.classList.remove('over');  // reset class during droping te column
-			}
-			function handleDrop(e) 
-			{
-
-			  if (e.stopPropagation) 
-			  {
-				e.stopPropagation(); // prevent element form return the default position
-			  }
-
-				// stop making the changes while grabbing the same element
-			  if (SrcElement != this) 
-			  {
-				SrcElement.innerHTML = this.innerHTML;
-				this.innerHTML = e.dataTransfer.getData('text/html');
-		      }
-			  return false;
-			}
-
-			function handleDragEnd(e) 
-			{
-
-			  [].forEach.call(columny, function (col) 
-			  {
-				col.classList.remove('over'); // remove 'over' class
-			  });
-			}
-			var columns = document.querySelectorAll('#columns .column'); //variable that clarify for which elements the DnD is done
-			//adding events and their type of movement
-			[].forEach.call(columns, function(events) 
-			{
-			  events.addEventListener('dragstart', handleDragStart, false);
-			  events.addEventListener('dragenter', handleDragEnter, false);
-			  events.addEventListener('dragover', handleDragOver, false);
-			  events.addEventListener('dragleave', handleDragLeave, false);
-			  events.addEventListener('drop', handleDrop, false);
-			  events.addEventListener('dragend', handleDragEnd, false); 
-			});
+                });
